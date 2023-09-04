f6552e895ed2a8c049b6b46d01456212baa35a8e
Ragavi987 <75991043+Ragavi987@users.noreply.github.com>
2023-09-04 11:20:55 +0530

    Update urls.py

--- Project/candidate/candidateapp/urls/urls.py
+++ Project/candidate/candidateapp/urls/urls.py
@@ -4,11 +4,11 @@ from candidateapp.views import *
 app_name = 'candidateapp'
 
 urlpatterns = [
-    path('', ApiOverview, name='candidateapp:home'),
-    path('candidates/', view_all_candidates, name='candidateapp:view_all_candidates'),
-    path('candidates/<int:pk>/', get_candidate_details, name='candidateapp:get_candidate_details'),
-    path('create/', add_candidate, name='candidateapp:add-new-candidate'),
-    path('update/<int:pk>/', update_candidate, name='candidateapp:update-candidate'),
-    path('delete/<int:pk>/', delete_candidate, name='candidateapp:delete-candidate'),
+    path('', ApiOverview, name='home'),
+    path('candidates/', view_all_candidates, name='view_all_candidates'),
+    path('candidates/<int:pk>/', get_candidate_details, name='get_candidate_details'),
+    path('create/', add_candidate, name='add-new-candidate'),
+    path('update/<int:pk>/', update_candidate, name='update-candidate'),
+    path('delete/<int:pk>/', delete_candidate, name='delete-candidate'),
 ]
 
